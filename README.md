4e61a00247891c03afaa4f6db714edfdf0d3932a
Danilo Taveira <dtnavega@gmail.com>
2024-02-15 14:31:05 -0300

    lógica do jogo (gesturedetector)

--- README.md
+++ README.md
@@ -1,16 +1,58 @@
-# jokenpo
+<h4 align="center"> 
+	🚧 JokenPo 🚧
+</h4>
 
-A new Flutter project.
+<p align="center">
+	<img alt="Status Concluído" src="https://img.shields.io/badge/STATUS-CONCLU%C3%8DDO-brightgreen">
+</p>
 
-## Getting Started
+<p align="center">
+ <a href="#-sobre-o-projeto">Sobre</a> •
+ <a href="#-layout">Layout</a> • 
+ <a href="#-como-executar-o-projeto">Como executar</a> • 
+ <a href="#user-content--licença">Licença</a>
+</p>
 
-This project is a starting point for a Flutter application.
 
-A few resources to get you started if this is your first Flutter project:
+## 💻 Sobre o projeto
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+📄 Foi desenvolvido no curso Desenvolvimento Android e IOS com Flutter - Crie 15 Apps de Jamilton Damasceno.
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+⚪ O projeto consiste no jogo JokenPo: o usuário clica em uma das opções de jogada (pedra, papel ou tesoura) e o app escolhe aleatoriamente uma opção de jogada também. Dessa forma, ocorre a validação, de acordo com as normas do jogo, e retorna o vencedor da jogada.
+
+
+---
+
+
+## 🎨 Layout
+
+### Emulador Android
+
+<p align="start">
+  <img alt="Nome do Projeto" title="Nome do Projeto" src="images/resultado.png" width="200px">
+</p>
+
+
+---
+
+
+### 📍 Como executar o projeto
+
+Antes de começar, você vai precisar ter instalado em sua máquina a linguagem Dart e o framework Flutter.
+Além disso, você também vai precisar ter configurado o emulador do Android Studio.
+
+---
+
+
+## 🛠 Tecnologias
+
+As seguintes ferramentas foram usadas na construção do projeto:
+
+-   **[Dart](https://dart.dev)**
+-   **[Flutter](https://flutter.dev)**
+
+---
+
+## 📝 Licença
+
+Feito por Danilo Taveira 👋🏽 [Entre em contato!](https://www.linkedin.com/in/danilo-taveira/)
--- lib/Jogo.dart
+++ lib/Jogo.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: file_names
+
+import 'dart:math';
 import 'package:flutter/material.dart';
+
 
 class Jogo extends StatefulWidget {
   const Jogo({super.key});
@@ -8,11 +12,62 @@ class Jogo extends StatefulWidget {
 }
 
 class _JogoState extends State<Jogo> {
+
+  var _imageApp = const AssetImage("images/padrao.png");
+  var _mensagem = "Escolha uma opção abaixo";
+
+   _opcaoSelecionada(String escolhaUsuario) {
+
+    var opcoes = ["pedra", "papel", "tesoura"];
+    var numero = Random().nextInt(3);
+    var escolhaApp = opcoes[numero];
+
+
+    switch (escolhaApp) {
+      case "pedra":
+        setState(() {
+          _imageApp = const AssetImage("images/pedra.png");
+        });
+        break;
+      case "papel":
+        setState(() {
+          _imageApp = const AssetImage("images/papel.png");
+        });
+        break;
+      case "tesoura":
+        setState(() {
+          _imageApp = const AssetImage("images/tesoura.png");
+        });
+        break;
+    }
+    if(
+      (escolhaUsuario == "pedra" && escolhaApp == "tesoura") ||
+      (escolhaUsuario == "tesoura" && escolhaApp == "papel") ||
+      (escolhaUsuario == "papel" && escolhaApp == "pedra")
+    ) {
+      setState(() {
+        _mensagem = "Parabéns! Você ganhou :)";
+      });
+    } else if(
+      (escolhaApp == "pedra" && escolhaUsuario == "tesoura") ||
+      (escolhaApp == "tesoura" && escolhaUsuario == "papel") ||
+      (escolhaApp == "papel" && escolhaUsuario == "pedra")
+    ) {
+      setState(() {
+        _mensagem = "Você perdeu :(";
+      });
+    } else {
+      setState(() {
+        _mensagem = "Empatamos ;)";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('JokenPo'),
+        title: const Text('JokenPo'),
         backgroundColor: Colors.blue,
       ),
       body: Column(
@@ -29,13 +84,13 @@ class _JogoState extends State<Jogo> {
               ),
             ),
           ),
-          Image.asset("images/padrao.png"),
-          const Padding(
-            padding: EdgeInsets.only(top: 32, bottom: 16),
+          Image(image: _imageApp),
+          Padding(
+            padding: const EdgeInsets.only(top: 72, bottom: 32),
             child: Text(
-              'Escolha uma opção abaixo',
+              _mensagem,
               textAlign: TextAlign.center,
-              style: TextStyle(
+              style: const TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),
@@ -44,9 +99,17 @@ class _JogoState extends State<Jogo> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Image.asset("images/pedra.png", height: 100),
-              Image.asset("images/papel.png", height: 100),
-              Image.asset("images/tesoura.png", height: 100),
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("pedra"),
+                child: Image.asset("images/pedra.png", height: 100),
+              ),
+              GestureDetector(
+                onTap: () =>
+                _opcaoSelecionada("papel"), child: Image.asset("images/papel.png", height: 100),
+              ),
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("tesoura"), child: Image.asset("images/tesoura.png", height: 100),
+              ),
             ],
           ),
         ],
